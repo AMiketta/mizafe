1ce6081dfb88eef37e96e3f453069808c725f629
Andreas Miketta <andreas.miketta@gmail,com>
2017-01-29 13:40:31 +0100

    removed background fix because it doesn’t work korrekt. added scripts for masonry

--- js/ajax_loading.js
+++ js/ajax_loading.js
@@ -1,35 +1,65 @@
-$("#content-link_start").click(function(){
+$("#content-link_start").click(function(event ) {
+  	event.preventDefault();
 	'use strict';
     $("#contenthere").load("content/test.html").fadeOut(500).fadeIn(500);
 });
 
-$("#content-link_kaffee").click(function(){
-	'use strict';
-    $("#contenthere").load("content/kaffee.html").fadeOut(500).fadeIn(500);
-	location.hash = "kaffee";
+$("#content-link_kaffee").click(function( evt ) {
+  	evt.preventDefault();
+  	evt = evt || window.event; // For IE
+    stopPropagation(evt);
+    $("#contenthere").fadeOut(500,function() {
+    	// Animation complete.
+    	$("#contenthere").load("content/kaffee.html", function() {
+
+    		
+
+  			$("#contenthere").fadeIn(500, function() {
+				// Activate Masonry Grid
+				$('.grid').masonry({
+  					itemSelector: '.grid-item',
+  					columnWidth: 160
+				});
+
+  			});
+		});
+  	});
+	//location.hash = "#kaffee";
 });
 
-$("#content-link_kaffee1").click(function(){
+$("#content-link_kaffee1").click(function(event ) {
+  	event.preventDefault();
 	'use strict';
     $("#contenthere").load("content/kaffee1.html").fadeOut(500).fadeIn(500); 
 	location.hash = "kaffee1";
 });
 
-$("#content-link_kaffee2").click(function(){
+$("#content-link_kaffee2").click(function(event ) {
+  	event.preventDefault();
 	'use strict';
     $("#contenthere").load("content/kaffee2.html").fadeOut(500).fadeIn(500); 
 	location.hash = "kaffee2";
 });
 
-$("#content-link_kaffee3").click(function(){
+$("#content-link_kaffee3").click(function(event ) {
+  	event.preventDefault();
 	'use strict';
     $("#contenthere").load("content/kaffee3.html").fadeOut(500).fadeIn(500);
 	location.hash = "kaffee3";
 });
 
-$("#content-link_kaffee4").click(function(){
+$("#content-link_kaffee4").click(function(event ) {
+  	event.preventDefault();
 	'use strict';
     $("#contenthere").load("content/kaffee4.html").fadeOut(500).fadeIn(500);  
 	location.hash = "kaffee4";
 });
+
+function stopPropagation(evt) {
+    if (typeof evt.stopPropagation == "function") {
+        evt.stopPropagation();
+    } else {
+        evt.cancelBubble = true;
+    }
+}
 
